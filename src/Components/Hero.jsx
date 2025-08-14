7f852c0be652415b0c14ba9e04da08f8ce07d572
GGameover <xverxai236@gmail.com>
2025-08-14 15:51:37 +0700

    Initial commit

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,7 @@
+export default function Hero(){
+    return (
+        <div className=" bg-blue-900 text-white">
+            <h1 className=" text-4xl">Frontend Developer</h1>
+        </div>
+    )
+}
