e6615b0db759a2cb7e1ef8add3a49eb39b7a37ab
GGameover <212758652+GGameover-AI@users.noreply.github.com>
2025-08-23 19:26:17 +0700

    Footer change row to col

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,13 +7,13 @@ export default function Footer() {
   return (
     <Element name="Footer">
       <footer className=" bg-[#0b0f14] text-slate-400 p-5 border-t border-white/10">
-        <div className="grid grid-cols-3 md:gap-5 px-7">
+        <div className="grid grid-cols-1 md:grid-cols-3 md:gap-5 px-7">
           <div className=" flex justify-center items-center gap-8">
             <img src={ReactLogo} className=" size-8 lg:size-12" />
             <img src={TailwindCSSLogo} className=" size-8 lg:size-12" />
           </div>
           <div className="flex flex-col text-center">
-            <h1 className="text-base lg:text-2xl font-bold mb-3">ติดต่อฉัน</h1>
+            <h1 className="text-base lg:text-2xl font-bold mb-3 mt-5 md:mt-0">ติดต่อฉัน</h1>
             <div className="flex justify-center gap-5">
               <a href="https://web.facebook.com/NeonzeroAi" className=" hover:text-[#00e6ff] ">
                 <FaFacebook className=" lg:size-8"/>
@@ -24,12 +24,12 @@ export default function Footer() {
             </div>
           </div>
           <div>
-            <h1 className=" text-base lg:text-2xl font-bold mb-3">อีเมลและเบอร์โทรศัพท์</h1>
+            <h1 className="flex flex-col items-center text-base lg:text-2xl font-bold mb-3 mt-5 md:mt-0">อีเมลและเบอร์โทรศัพท์</h1>
             <div>
-              <p className="mb-2 flex gap-3 items-center text-sm lg:text-lg">
+              <p className="mb-2 flex flex-col gap-3 items-center md:items-start text-sm lg:text-lg">
                 <FaEnvelope className=" lg:size-6 sm:block hidden" /> xverxai236@gmail.com
               </p>
-              <p className="mb-2 flex gap-3 items-center text-sm lg:text-lg" >
+              <p className="mb-2 flex flex-col gap-3 items-center md:items-start text-sm lg:text-lg" >
                 <FaPhone className=" lg:size-6 sm:block hidden"/> +66952799702
               </p>
             </div>
