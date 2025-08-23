fb92c62c3ea091e78b5d1a45cbfcbe0d120e1369
GGameover <212758652+GGameover-AI@users.noreply.github.com>
2025-08-23 19:04:49 +0700

    Projects

--- src/Components/Projects/SimpleTodolist.jsx
+++ src/Components/Projects/SimpleTodolist.jsx
@@ -9,13 +9,13 @@ import todolistCapture from "../../assets/ImagesProjects/todoCapture.png"
 
 export default function SimpleTodolist() {
   return (
-    <div className="flex flex-col px-8 py-6 rounded-lg border-1 border-cyan-400 shadow-cyan-400/80 shadow-lg">
+    <div className="flex flex-col px-4 py-2 md:px-8 md:py-6 rounded-lg border-1 border-cyan-400 shadow-cyan-400/80 shadow-lg">
       {/* ชื่อ Project */}
       <h2 className=" font-bold text-center mb-5">SimpleTodolist</h2>
       {/* รูป Project */}
       <img src={todolistCapture} className="mb-2 shadow shadow-black hover:scale-105 hover:shadow-md transition" />
       {/* คำอธิบาย */}
-      <p className="mb-5">
+      <p className="mb-5 text-base">
         การทำเว็บ todolist อย่างง่ายเพื่อศึกษาทำความเข้าใจ CRUD และ RESTful API ระหว่าง Frontend กับ Backend และใช้ MongoDB เพื่อเก็บข้อมูลด้วย ODM(Moongosedb)
       </p>
       {/* ปุ่มลิงค์และTechStack */}
@@ -25,15 +25,15 @@ export default function SimpleTodolist() {
             href="https://github.com/GGameover-AI/Todo_List"
             className=" bg-pink-500 px-6 py-2 rounded-2xl hover:bg-pink-400 hover:scale-110 hover:shadow-md hover:shadow-pink-400 transition"
           >
-            <img src={githubIcon} className=" w-8 h-8" />
+            <img src={githubIcon} className=" w-6 h-6 md:w-8 md:h-8" />
           </a>
         </div>
         <div className="flex justify-end items-center gap-1">
-          <img src={reactIcon} className="w-12 h-12" />
-          <img src={bootstrapIcon} className="w-12 h-12" />
-          <img src={nodejsIcon} className="w-12 h-12" />
-          <img src={expressIcon} className="w-12 h-12" />
-          <img src={mongodbIcon} className="w-12 h-12" />
+          <img src={reactIcon} className="w-8 h-8 md:w-12 md:h-12" />
+          <img src={bootstrapIcon} className="w-8 h-8 md:w-12 md:h-12" />
+          <img src={nodejsIcon} className="w-8 h-8 md:w-12 md:h-12" />
+          <img src={expressIcon} className="w-8 h-8 md:w-12 md:h-12" />
+          <img src={mongodbIcon} className="w-8 h-8 md:w-12 md:h-12" />
         </div>
       </div>
     </div>
